c63110051c5de26a5d6d16b40da4b74aef5ff9c6
PriyanshSoniiii <priyansh.soni@intimetec.com>
2025-07-07 20:48:33 +0530

    restructured and removed EmailController and NewsArticleRepo

--- NewsAggregation.Server/Repository/Interfaces/INewsArticleRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿//using NewsAggregation.Server.Models.Entities;
-
-//namespace NewsAggregation.Server.Repository.Interfaces
-//{
-//    public class INewsArticleRepository
-//    {
-//        //Task SaveArticlesAsync(IEnumerable<NewsArticle> articles, CancellationToken cancellationToken);
-//    }
-//}
